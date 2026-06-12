1731132a540dc1ace2d6fbe4c0c14a18af616894
Anshika Aggarwal <anshikaagarwal9966@gmail.com>
2026-06-12 19:16:19 +0530

    Add files via upload

---
+++ DATATYPES_AND_VARIABLES/intro.js
@@ -0,0 +1,66 @@
+console.log("welcome mitron!")
+
+// let first_Name = "Anshika Aggarwal"
+// first_Name = "Anshika"
+// first_Name = true //boolean value can also be assigned to the same variable as JavaScript is a dynamically typed language
+
+// emp_Name = prompt("Enter your Name")
+// if(emp_Name){
+//     console.log("welcome user :",emp_Name);
+// }
+// else {
+//     console.log("please enter your name");
+// }
+
+// console.log(first_Name);
+
+// let $age = 25
+// console.log($age);
+
+// let PI = 3.14 //constants k liye naming conventions capital letter use krte hai
+// PI = 3.1415 //error as we cannot change the value of a constant variable
+// console.log(PI);
+
+// let collegeName = "MIT"// strings
+// collegeName = 'IIT' //character
+// console.log(collegeName);
+
+// let job_Status = false //boolean
+// console.log(job_Status);
+
+//undefined
+
+// let age;
+// console.log(age); 
+//undefined as we have not assigned any value to the variable age
+// age = 25
+// console.log(age);
+
+// let city = null
+// console.log(city); //null as we have assigned null value to the variable city
+
+// typeof // data -> datatype
+// console.log(typeof 25);
+// console.log(typeof "Anshika");
+// console.log(typeof 5.6);
+// console.log(typeof true);
+// console.log(typeof undefined);
+// console.log(typeof null); //object is the output as null is considered as an object in JavaScript
+
+// VAR VARIABLE EXAMPLE
+// var score = 100;
+// console.log(score);
+// var score = 200;
+// console.log(score);
+// var score = 300;
+// console.log(score);
+
+function print() {
+    var square = number * number
+    // undefined * undefined = NAN
+    console.log(square);
+    var number = 50;
+    var sum = number + number;
+    console.log(sum);
+}
+print();
