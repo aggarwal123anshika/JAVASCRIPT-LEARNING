c994c7c74fdf6586f0af9ee4e21520a08fad4e79
Anshika Aggarwal <anshikaagarwal9966@gmail.com>
2026-06-13 19:08:53 +0530

    Add files via upload

---
+++ FUNCTIONS/functions.js
@@ -0,0 +1,23 @@
+console.log("welcome to tutedude");
+
+
+function Logger() {
+    console.log("Hello ji mitro!!");
+    console.log("aaj masum bhaut acha hai🕺")
+}
+Logger(); //function calling,invoke,run,etc.
+
+// function declarations and // function definations
+
+// function k signature ko function declarations bolte hai or jb hm ek fucntion ko banate h or template banate hai
+// function ki body ko function definations bolte hai
+// function signature -> parameter
+
+let electricity = true //global scope we can access this varianble anywhere in this code
+function Juicer(apples, oranges) {
+    // parameters are local variables like apples and oranges
+    let juice = `juice with ${apples} apples and ${oranges} oranges`
+    return juice
+}
+const glass = Juicer(3,4)
+console.log(glass);
