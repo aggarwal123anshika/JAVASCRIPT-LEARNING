4a76b2b968c3e8dd016f9662c0a41a6cde88f32c
Anshika Aggarwal <anshikaagarwal9966@gmail.com>
2026-06-13 19:09:35 +0530

    Add files via upload

---
+++ CONDITIONAL_STATEMENTS/conditional.js
@@ -0,0 +1,17 @@
+console.log("welcome mitro!");
+
+// can you drive
+let vision_test = false;
+if(vision_test == true) {
+    console.log("yes you can drive");
+}
+console.log("end of code");
+
+// if - else statement example
+let Do_You_Have_Food = true
+if(Do_You_Have_Food == true) {
+    console.log("have sweets please")
+}
+else {
+    console.log("first eat the food now go")
+}
