9b5d7379f83f923189263b72babd98205cf40416
Anshika Aggarwal <anshikaagarwal9966@gmail.com>
2026-06-24 10:37:47 +0530

    Add files via upload

---
+++ DOM/dom.js
@@ -0,0 +1,26 @@
+// let heading = document.getElementById("heading1");
+// console.log(heading);
+
+// let headings = document.getElementById("heading2");
+// console.log(headings);
+
+// let p = document.getElementsByClassName("sample");
+// console.log(p);
+
+// let element = document.getElementsByTagName("p");
+// console.log(element);
+
+// let element = document.querySelector("#heading2")
+// console.log(element);
+
+// let element = document.querySelector(".sample")
+// console.log(element);
+
+// let tags = document.querySelector("p")
+// console.log(tags);
+
+let data = document.querySelectorAll("p")
+console.log(data);
+
+let data1 = document.querySelectorAll(".sample")
+console.log(data1);
