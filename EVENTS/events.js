1890315e8f6ed01861d0227d81d375d76e4a020b
Anshika Aggarwal <anshikaagarwal9966@gmail.com>
2026-06-28 12:01:15 +0530

    Add files via upload

--- EVENTS/events.js
+++ EVENTS/events.js
@@ -15,16 +15,60 @@
 
 // MODERN WAY OF ONCLICK IN EVENTS
 
-const button = document.querySelector("#btn2");
+// const button = document.querySelector("#btn2");
 
-function greet() {
-    alert("Welcome user!!")
-}
-function changeColor() {
-    button.style.backgroundColor = "lightgreen";
-    button.style.borderRadius = "50px";
-}
+// function greet() {
+//     alert("Welcome user!!")
+// }
+// function changeColor() {
+//     button.style.backgroundColor = "lightgreen";
+//     button.style.borderRadius = "50px";
+// }
 
-// button.addEventListener("click",greet);
-button.addEventListener("click", greet);
-button.addEventListener("click", changeColor);
+// // button.addEventListener("click",greet);
+// button.addEventListener("click", greet);
+// button.addEventListener("click", changeColor);
+
+// function printMessage() {
+//     let MsgBox = document.querySelector("#message");
+//     let msg = document.createElement("h2");
+//     msg.innerText = "Response Submitted";
+//     MsgBox.append(msg);
+// }
+// btn2.addEventListener("click", printMessage);
+
+
+// let logIn = document.querySelector("#login");
+// document.querySelector("#details").style.display = "none";
+
+// function application() {
+//     let screen = document.querySelector("#details");
+//     screen.style.display = "block";
+//     let salary = prompt("Enter your salary");
+//     let salaryEl = document.createElement("h2");
+//     salaryEl.innerText = "Your given salary is " + salary;
+//     screen.append(salaryEl);
+
+//     let bonus = document.getElementById("details");
+//     bonus.addEventListener("click", function() {
+//         // let bonusSalary = salary;
+//         // bonusSalary = bonusSalary + 2000;
+//         salary += 2000;
+//         // console.log(salary);
+//         //  let salaryEl = document.createElement("h2");
+//          salaryEl.innerText = "Your given salary after bonus is: " + salary;
+//         //  screen.append(salaryEl);
+//     });
+// }
+// logIn.addEventListener("click", application);
+
+// CHANGE PARKING ZONE COLOR
+
+const Box = document.querySelector(".box");
+
+Box.addEventListener("mouseover", () => {
+    Box.style.background = 'lightgreen';
+})
+Box.addEventListener("mouseout", () => {
+    Box.style.background = 'red';
+})
